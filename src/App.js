4c22f2a5ab74ae8d09c60e3d6f0703ddc35e34db
vinodkumar <vinodkumarc881@gmail.com>
2024-05-10 09:29:30 +0530

    React-B4

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { Portfolio } from './components/Portfolio';
+import { Service } from './components/service';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Service/>
+      <Portfolio/>
+    </div>
+  );
+}
+
+export default App;
